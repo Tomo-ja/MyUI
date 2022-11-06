aa19b117060deffd97a53500faf5a66125842a50
Tomo-ja <tomonao.hj@gmail.com>
2022-11-06 12:33:54 -0800

    feat: finish colors generator

--- components/generator/colors/Converter.tsx
+++ components/generator/colors/Converter.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faClone } from '@fortawesome/free-regular-svg-icons'
 import StyledConvertor from './converter.styles'
 
 
-const hexToRgb = (hex: string) => {
+const hexToRgb = (hex: string): number[]=> {
 	const rgbArray =   hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i, (m, r, g, b) => '#' + r + r + g + g + b + b)
 											.substring(1).match(/.{2}/g)!
 											.map(x => parseInt(x, 16))
@@ -19,7 +19,7 @@ const rgbToHex = (r: number, g: number, b: number) => {
 	return hexString
 }
 
-const hexToHsl = (hex: string) => {
+const hexToHsl = (hex: string): string => {
 	const rgb = hexToRgb(hex)
 	const r = rgb[0] / 255
 	const g = rgb[1] / 255
@@ -57,9 +57,82 @@ const hexToHsl = (hex: string) => {
 	return 'hsl(' + h + ', ' + s + '%, ' + l + '%)'
 }
 
+const hslToHex = (h: number, s: number, l: number): string => {
+  l /= 100;
+  const a = s * Math.min(l, 1 - l) / 100;
+  const f = (n: number) => {
+    const k = (n + h / 30) % 12
+    const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1)
+    return Math.round(255 * color).toString(16).padStart(2, '0')
+  };
+  return `#${f(0)}${f(8)}${f(4)}`;
+}
+
+enum Option {
+	hex, rgb, hsl
+}
+
 const Convertor = () => {
 
 	const [ color, setColor ] = useState('#1890ff')
+	const hexRef = useRef<HTMLInputElement>(null)
+	const rgbRef = useRef<HTMLInputElement>(null)
+	const hslRef = useRef<HTMLInputElement>(null)
+
+	const onChangeHex = (hex: string) => {
+		if(!hexRef.current || !rgbRef.current || !hslRef.current) return
+		rgbRef.current.value = 'rgb(' + hexToRgb(hex).join(', ') + ')'
+		hslRef.current.value = hexToHsl(hex)
+		setColor(hex)
+	}
+
+	const onChangeRgb = (rgb: string) => {
+		if(!hexRef.current || !rgbRef.current || !hslRef.current) return
+		const rgbArray = rgb.slice(4, (rgb.length)).slice(0, -1).replace(/\s/g, '').split(',').map(num => Number(num))
+		const hex = rgbToHex(rgbArray[0], rgbArray[1], rgbArray[2])
+
+		hexRef.current.value = hex
+		hslRef.current.value = hexToHsl(hex)
+
+		setColor(hex)
+	}
+
+	const onChangeHsl = (hsl: string) => {
+		if(!hexRef.current || !rgbRef.current || !hslRef.current) return
+		const hslArray = hsl.slice(4, (hsl.length)).slice(0, -1).replace(/\s/g, '').replace(/%/g, '').split(',').map(num => Number(num))
+		const hex = hslToHex(hslArray[0], hslArray[1], hslArray[2])
+		rgbRef.current.value = 'rgb(' + hexToRgb(color).join(', ') + ')'
+		hexRef.current.value = hex
+		setColor(hex)
+	}
+
+	const copyColor = (type: Option) => {
+		switch(type) {
+			case Option.hex: 
+				navigator.clipboard.writeText(hexRef.current!.value)
+				break
+			case Option.hsl: 
+				navigator.clipboard.writeText(hslRef.current!.value)
+				break
+			case Option.rgb: 
+				navigator.clipboard.writeText(rgbRef.current!.value)
+				break
+		}
+
+	}
+
+	const onKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
+		if(e.key === 'Enter') {
+			e.currentTarget.blur()
+		}
+	}
+	
+	useEffect(()=> {
+		if(!hexRef.current || !rgbRef.current || !hslRef.current) return
+		hexRef.current.value = color
+		rgbRef.current.value = 'rgb(' + hexToRgb(color).join(', ') + ')'
+		hslRef.current.value = hexToHsl(color)
+	}, [])
 
 
 	return (
@@ -67,18 +140,41 @@ const Convertor = () => {
 			<div className='option'>
 				<div className='option__item'>
 					<p>HEX</p>
-					<input type="text" />
-					<FontAwesomeIcon icon={faClone} size='lg' color='#8c8c8c'/>
-				</div>
-				<div className='option__item'>
-					<p>HSL</p>
-					<input type="text" />
-					<FontAwesomeIcon icon={faClone} size='lg' color='#8c8c8c'/>
+					<input 
+						ref={hexRef}
+						type="text" 
+						onBlur={(e) => onChangeHex(e.target.value)}
+						onKeyUp={(e) => onKeyUp(e)}
+					/>
+					<FontAwesomeIcon icon={faClone} size='lg' color='#8c8c8c'
+						onClick={() => copyColor(Option.hex)}
+					/>
 				</div>
 				<div className='option__item'>
 					<p>RGB</p>
-					<input type="text" />
-					<FontAwesomeIcon icon={faClone} size='lg' color='#8c8c8c'/>
+					<input 
+						ref={rgbRef}
+						type="text" 
+						onKeyUp={(e) => onKeyUp(e)}
+						onBlur={(e) => onChangeRgb(e.target.value)}
+					/>
+					<FontAwesomeIcon 
+						icon={faClone} size='lg' color='#8c8c8c'
+						onClick={() => copyColor(Option.rgb)}
+					/>
+				</div>
+				<div className='option__item'>
+					<p>HSL</p>
+					<input
+						ref={hslRef}
+						type="text" 
+						onKeyUp={(e) => onKeyUp(e)}
+						onBlur={(e) => onChangeHsl(e.target.value)}
+					/>
+					<FontAwesomeIcon 
+						icon={faClone} size='lg' color='#8c8c8c'
+						onClick={() => copyColor(Option.hsl)}
+					/>
 				</div>
 			</div>
 			<div className='preview'>
